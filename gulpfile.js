b2b8fa39a048f1f0a4bbf84e2cbcd2f90743b4b7
pavilion <yifeiting@gmail.com>
2016-10-28 12:11:42 +0800

    add build-dll and build-dev-dll task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var webpack = require('webpack');
 var webpackDevServer = require('webpack-dev-server');
 
 var makeConfig = require('./make-webpack.config');
+var makeDllConfig = require('./webpack.dll.config');
+
 var excludeFromStats = [
     /node_modules[\\\/]/
 ];
@@ -11,31 +13,63 @@ var excludeFromStats = [
 gulp.task('default', function(callback){
 
   var compiler = webpack(makeConfig({debug: true}), function(err, stats){
-    console.log(stats.toString());
     new webpackDevServer(compiler, {
       publicPath: compiler.options.output.publicPath,
       hot: true,
-      historyApiFallback: true,
-      quiet: false,
+      historyApiFallback: false,
       contentBase: '__build/',
       stats: {
           cached: false,
           exclude: excludeFromStats,
           colors: true,
+      },
+      proxy: {
+        '/api/*': {
+          target: "http://wx.hexugongshe.com",
+          //target: "http://wljbi.chinacloudapp.cn",
+          secure: false,
+          changeOrigin: true,
+          rewrite: function(req) {
+
+          }
+        },
+        '*.htm[l]?': {
+          target: "http://localhost:3000",
+          rewrite: function(req) {
+            console.log(new Date().getTime() + " [" + req.headers.method + "] " + req.url);
+            req.url = "/";
+          }
+        }
+
       }
-      
-    }).listen(3000, '127.0.0.1', function(err){
+
+    }).listen(3000, '0.0.0.0', function(err){
       if(err) throw new gutil.PluginError("webpack-dev-server", err);
-      gutil.log("[webpack-dev-server]", "http://localhost:3000/webpack-dev-server/");
+      gutil.log("[webpack-dev-server]", "http://0.0.0.0:3000/webpack-dev-server/");
     })
   });
-  
+
   callback();
 })
 
-gulp.task('build', function(callback){
+/**
+ * 产品构建
+ * @type
+ */
+gulp.task('build',[], function(){
   webpack(makeConfig({debug: false}), function(err, stats){
     console.log(stats.toString());
   });
-  callback()
+})
+
+gulp.task("build-dll", function(){
+  webpack(makeDllConfig({debug: false}), function(err, stats){
+    console.log(stats.toString());
+  })
+})
+
+gulp.task("build-dev-dll", function(){
+  webpack(makeDllConfig({debug: true}), function(err, stats){
+    console.log(stats.toString());
+  })
 })
